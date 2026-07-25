5ce17cec9dc54b244e5c0ed0216c6f488f122977
Md. Fazle Rabbi <fazle.rabbi.mugdho@gmail.com>
2026-07-25 11:24:19 +0600

    chore(firmware): verify esp32 core 3.x toolchain via platformio

---
+++ firmware/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/Md.\ Fazle\ Rabbi/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/Md.\ Fazle\ Rabbi/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
